03d4c64aa4df504c011849eaacf2a0f78582f233
Daniel Bañuelos <dbanuelos.1324@gmail.com>
2024-10-09 20:54:22 -0600

    changed from vercel storage postgresql to neondb postgresql

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -1,6 +1,11 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { createPool } from '@vercel/postgres';
-import { POSTGRES_URL } from '$env/static/private';
+// import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { drizzle } from "drizzle-orm/neon-http";
+// import { createPool } from '@vercel/postgres';
+import { neon } from "@neondatabase/serverless";
+import { NEON_POSTGRES_URL } from '$env/static/private';
 
-const pool = createPool({ connectionString: POSTGRES_URL });
-export const db = drizzle(pool);
+// const pool = createPool({ connectionString: POSTGRES_URL });
+// export const db = drizzle(pool);
+
+const sql = neon(NEON_POSTGRES_URL);
+export const db = drizzle(sql);
